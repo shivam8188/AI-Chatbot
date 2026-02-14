04d3fe776003f1b794e023d58d7e2ed37c530cc9
shivam8188 <shivamkumar818894@gmail.com>
2026-02-14 19:57:11 +0530

    Update server configuration for live deployment
    Comment out the port variable and update the server log to display the live URL instead.

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 const app = express();
-const port = 3001;
-
+//const port = 3001;
+const liveurl = "https://ai-chatbot-frontend-e4bb.onrender.com/"
 app.use(cors());
 app.use(express.json());
 
@@ -28,7 +28,8 @@ app.post('/chat', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  //console.log(`Server is running at http://localhost:${port}`);
+  console.log(liveurl);
 });
 
 
